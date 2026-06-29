fb2aaf265c16130bb4a2e94cd99e670b14ed4c03
ohm-vishwa <ohmdto1513@gmail.com>
2026-06-29 21:36:01 +0530

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+function connectToDB() {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("Server is connected to DB");
+    })
+    .catch((err) => {
+      console.log("Error connecting to DB");
+      process.exit(1);
+    });
+}
+
+module.exports = connectToDB;
